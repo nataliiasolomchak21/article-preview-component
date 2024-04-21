761b34fbfe9a48d3cb5991181aa4d44da9a890ef
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-04-21 19:07:23 +0100

    feat: Add article component and share functionality

---
+++ script.js
@@ -0,0 +1,7 @@
+const shareBtn = document.querySelector('.share-arrow')
+const shareBox = document.querySelector('.share-hidden')
+
+
+shareBtn.addEventListener("click", function() {
+    shareBox.classList.add('active')
+})
